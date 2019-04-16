69e4a5e3aaa5d8a707a025052bb8fd0b82203271
vtrev <baloyi.trev@gmail.com>
2019-04-16 15:28:59 +0200

    removed old stuff,added slider on footer and new paragraph

--- js/dom.js
+++ js/dom.js
@@ -1,86 +1,126 @@
- //fetch the repos from github
- let fetchRepos = async () => {
-     let response = await fetch('https://api.github.com/users/vtrev/repos');
-     let repos = await response.json();
-     return repos
- };
+document.addEventListener('DOMContentLoaded', function () {
 
- //sort them out and take only the data needed
- let makeRepos = async (repos) => {
-     let frontEndProjects = [];
-     let backEndProjects = [];
-     let herokuLinks = {
-         "waiters-app": "https://vtrev-waiters.herokuapp.com/waiters/waiterNameHere",
-         "shoe-store": "https://vtrev-shoesapi.herokuapp.com/",
-         "settings-bill-expressjs": "https://vtrev-settings-bill.herokuapp.com/",
-         "registrations-webapp": "https://vtrev-registrations.herokuapp.com/",
-         "greetings-webapp": "https://vtrev-greetings.herokuapp.com/"
-     }
-     //seperate the repos into back and front-end
-     repos.filter((repo) => {
-         let description = repo.description;
-         if (description) {
-             let tmpRepo = {
-                 'heading': repo.name,
-                 'description': description.substr(0, description.length - 2)
-             }
-             let descriptor = description.slice(-2);
-             if (descriptor == '**') {
-                 let herokuKeys = Object.keys(herokuLinks);
-                 herokuKeys.filter((repoName) => {
-                     if (repoName == repo.name) {
-                         tmpRepo.link = herokuLinks[repoName];
-                         backEndProjects.push(tmpRepo);
-                     }
-                 });
-             };
-             if (descriptor == '.*') {
-                 let tmpRepo = {
-                     'heading': repo.name,
-                     'description': description.substr(0, description.length - 1)
-                 };
-                 if (repo.default_branch == "gh-pages") {
-                     tmpRepo.link = `https://vusibaloyi.xyz/${repo.name}`
-                 }
-                 frontEndProjects.push(tmpRepo)
-             };
-         };
+    // handlebars to compile before carousel init
+    let generateFooterSliderContent = function (techList) {
+        let footerCarouselElement = document.getElementById("footer-carousel");
+        let footerTechTemplateSource = document.getElementById("footer-tech-template").innerHTML;
+        let footerTechTemplate = Handlebars.compile(footerTechTemplateSource);
+        footerCarouselElement.innerHTML = footerTechTemplate(techList);
+    };
+    let techList = ['css', 'express', 'git', 'hb', 'heroku', 'html', 'js', 'mocha', 'mysql', 'nodejs', 'pg', 'rest', 'travis'];
 
-     });
-     return {
-         frontEndProjects,
-         backEndProjects
-     }
- };
- let populateProjects = (projects, type) => {
+    generateFooterSliderContent({
+        techList
+    });
 
-     let projectsData = {
-         projects
-     };
+    // footer carousel
+    var elems = document.querySelector('#footer-carousel');
+    var instances = M.Carousel.init(elems, {
+        dist: 0,
+        padding: 50,
+        duration: 1500,
+        numVisible: 3
+    });
 
-     let frontEndElement = document.getElementById("front-end");
-     let backEndElement = document.getElementById("back-end")
-     let projectsTemplateSource = document.getElementById("projects-template").innerHTML;
-     let projectsTemplate = Handlebars.compile(projectsTemplateSource);
-     let projectsHTML = projectsTemplate(projectsData);
-     if (type == 'frontEnd') {
-         frontEndElement.innerHTML = projectsHTML;
-     }
-     if (type == 'backEnd') {
-         backEndElement.innerHTML = projectsHTML;
-     }
- }
+    var slideFooterCarousel = () => {
+        let carousel = document.getElementById('footer-carousel');
+        instance = M.Carousel.getInstance(carousel);
+        instance.next();
+        setTimeout(slideFooterCarousel, 2000);
+    }
+    slideFooterCarousel();
+
+    // =========================================== non materialize js ===================================================== //
+    //fetch the repos from github
+    let fetchRepos = async () => {
+        let response = await fetch('https://api.github.com/users/vtrev/repos');
+        let repos = await response.json();
+        return repos
+    };
+
+    //sort them out and take only the data needed
+    let makeRepos = async (repos) => {
+        let frontEndProjects = [];
+        let backEndProjects = [];
+        let herokuLinks = {
+            "waiters-app": "https://vtrev-waiters.herokuapp.com/waiters/waiterNameHere",
+            "shoe-store": "https://vtrev-shoesapi.herokuapp.com/",
+            "settings-bill-expressjs": "https://vtrev-settings-bill.herokuapp.com/",
+            "registrations-webapp": "https://vtrev-registrations.herokuapp.com/",
+            "greetings-webapp": "https://vtrev-greetings.herokuapp.com/"
+        }
+        //seperate the repos into back and front-end
+        repos.filter((repo) => {
+            let description = repo.description;
+            if (description) {
+                let tmpRepo = {
+                    'heading': repo.name,
+                    'description': description.substr(0, description.length - 2)
+                }
+                let descriptor = description.slice(-2);
+                if (descriptor == '**') {
+                    let herokuKeys = Object.keys(herokuLinks);
+                    herokuKeys.filter((repoName) => {
+                        if (repoName == repo.name) {
+                            tmpRepo.link = herokuLinks[repoName];
+                            backEndProjects.push(tmpRepo);
+                        }
+                    });
+                };
+                if (descriptor == '.*') {
+                    let tmpRepo = {
+                        'heading': repo.name,
+                        'description': description.substr(0, description.length - 1)
+                    };
+                    if (repo.default_branch == "gh-pages") {
+                        tmpRepo.link = `https://vusibaloyi.xyz/${repo.name}`
+                    }
+                    frontEndProjects.push(tmpRepo)
+                };
+            };
+
+        });
+        return {
+            frontEndProjects,
+            backEndProjects
+        }
+    };
+
+    // hadnlebars  
+    let populateProjects = (projects, type) => {
+
+        let projectsData = {
+            projects
+        };
+
+        let frontEndElement = document.getElementById("front-end");
+        let backEndElement = document.getElementById("back-end")
+        let projectsTemplateSource = document.getElementById("projects-template").innerHTML;
+        let projectsTemplate = Handlebars.compile(projectsTemplateSource);
+        let projectsHTML = projectsTemplate(projectsData);
+        if (type == 'frontEnd') {
+            frontEndElement.innerHTML = projectsHTML;
+        }
+        if (type == 'backEnd') {
+            backEndElement.innerHTML = projectsHTML;
+        }
+    }
 
 
- fetchRepos()
-     .then((response) => {
-         return makeRepos(response);
-     })
-     .then(
-         (data) => {
-             let frontEndList = data.frontEndProjects.reverse();
-             let backEndList = data.backEndProjects.reverse();
-             populateProjects(frontEndList, 'frontEnd');
-             populateProjects(backEndList, 'backEnd');
-         }
-     );
+
+
+
+    // fetchRepos()
+    //     .then((response) => {
+    //         return makeRepos(response);
+    //     })
+    //     .then(
+    //         (data) => {
+    //             let frontEndList = data.frontEndProjects.reverse();
+    //             let backEndList = data.backEndProjects.reverse();
+    //             populateProjects(frontEndList, 'frontEnd');
+    //             populateProjects(backEndList, 'backEnd');
+    //         }
+    //     );
+
+});
