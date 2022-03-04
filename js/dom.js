f40cb99b23acfade6dd788f56df7a9124c7ccc21
Vusi <32287425+vtrev@users.noreply.github.com>
2022-03-04 12:55:33 +0200

    Update dom.js
    Added expansior production link, gh-pages out of sync.

--- js/dom.js
+++ js/dom.js
@@ -52,7 +52,8 @@ document.addEventListener('DOMContentLoaded', function () {
             "shoe-store": "https://vtrev-shoesapi.herokuapp.com/",
             "settings-bill-expressjs": "https://vtrev-settings-bill.herokuapp.com/",
             "registrations-webapp": "https://vtrev-registrations.herokuapp.com/",
-            "greetings-webapp": "https://vtrev-greetings.herokuapp.com/"
+            "greetings-webapp": "https://vtrev-greetings.herokuapp.com/",
+            "expansiorwebsite":"https://expansior.co.za/"
         }
 
         repos.filter((repo) => {
