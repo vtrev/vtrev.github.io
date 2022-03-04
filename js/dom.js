e53367a3fbd8abde2e7203c2d7d6795827415a46
Vusi <32287425+vtrev@users.noreply.github.com>
2022-03-04 12:46:39 +0200

    Update dom.js
    Removed stockman url from heroku links

--- js/dom.js
+++ js/dom.js
@@ -52,8 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
             "shoe-store": "https://vtrev-shoesapi.herokuapp.com/",
             "settings-bill-expressjs": "https://vtrev-settings-bill.herokuapp.com/",
             "registrations-webapp": "https://vtrev-registrations.herokuapp.com/",
-            "greetings-webapp": "https://vtrev-greetings.herokuapp.com/",
-            "stock-man":"https://vusi.world/stock-management/index.html"
+            "greetings-webapp": "https://vtrev-greetings.herokuapp.com/"
         }
 
         repos.filter((repo) => {
